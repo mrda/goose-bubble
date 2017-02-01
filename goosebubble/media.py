d98a3eb5c51bf13c24ca28879147d36c0ed73a3b
Michael Davies <michael@the-davies.net>
2017-02-01 12:12:42 +1030

    Adding standalone parsing to media.py

--- goosebubble/media.py
+++ goosebubble/media.py
@@ -20,47 +20,94 @@
 # 02111-1307, USA.
 #
 import exifread
+import re
+import sys
+import os
+
+SPLIT_STR = ':| '
 
 
-MEDIA_EXTENSIONS = ['jpeg', 'jpg', 'png', 'cr2', 'mov']
-
-
-def get_date_for_image_file(filename):
+def get_date_for_image(filename, only_year=False):
     """Return the date for an image"""
     fd = open(filename, 'rb')
     try:
         tags = exifread.process_file(fd)
     except UnicodeEncodeError:
-        print("*** Problem processing file '%s'" % filename)
+        print("** Problem processing file '%s'" % filename)
         return None
 
     if tags is None:
-        print("*** File '%s' has no exif data") % filename
+        print("** File '%s' has no exif data") % filename
         return False
 
     fd.close()
 
     # DateTimeOriginal
     try:
-        return str(tags['EXIF DateTimeOriginal']).split(':')[0]
+        datestr = re.split(SPLIT_STR, str(tags['EXIF DateTimeOriginal']))
+        if only_year:
+            return datestr[0]
+        else:
+            return datestr
     except KeyError:
         pass
 
     # DateTime
     try:
-        return str(tags['Image DateTime']).split(':')[0]
+        datestr = re.split(SPLIT_STR, str(tags['Image DateTime']))
+        if only_year:
+            return datestr[0]
+        else:
+            return datestr
     except KeyError:
         pass
 
     # DateTimeDigitized
     try:
-        return str(tags['EXIF DateTimeDigitized']).split(':')[0]
+        datestr = re.split(SPLIT_STR, str(tags['EXIF DateTimeDigitized']))
+        if only_year:
+            return datestr[0]
+        else:
+            return datestr
     except KeyError:
         pass
 
-    return False
+    if only_year:
+        return "no-year"
+    else:
+        return "no-date"
+
+
+def get_year_for_image(filename):
+    return get_date_for_image(filename, only_year=True)
+
+
+def test_get_date_for_files(directory, all_files=False, only_year=False):
+    for subdir, dirs, files in os.walk(directory):
+        count = last_count = 0
+        exif = ""
+        fullpath = ""
+        print("Entering subdirectory %s" % subdir)
+        for f in files:
+            count += 1
+            fullpath = os.path.join(subdir, f)
+            exif = get_date_for_image(fullpath)
+            if (all_files or (count % 1000 == 0)):
+                print("-- Examining %dth file %s, exif = %s" %
+                      (count, fullpath, exif))
+                last_count = count
+        if count != last_count:
+            print("-- Processing %dth file %s, exif = %s" %
+                  (count, fullpath, exif))
 
 
 if __name__ == '__main__':
-    # TODO(mrda): Provide some standalone parsing here
-    pass
+    if len(sys.argv) < 2:
+        print("%s: [<files>][<directories>]" % sys.argv[0])
+    for elem in sys.argv[1:]:
+        if os.path.isfile(elem):
+            print("%s: %s" % (elem, get_date_for_image(elem, only_year=False)))
+        elif os.path.isdir(elem):
+            test_get_date_for_files(elem, all_files=True, only_year=False)
+        else:
+            print("*** %s isn't a file or directory" % elem)
